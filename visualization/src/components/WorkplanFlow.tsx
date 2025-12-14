332a6a021ff557af6aa9b7dd14265afc5b6d8a5a
Giridhar <jagged3dges@gmail.com>
2025-12-15 01:11:37 +0530

    fix: show full reactflow controls set

--- visualization/src/components/WorkplanFlow.tsx
+++ visualization/src/components/WorkplanFlow.tsx
@@ -328,7 +328,7 @@ const WorkplanFlow = ({
         )}
         
         {/* Controls - responsive */}
-        <Controls style={controlsStyle} />
+        <Controls style={controlsStyle} showZoom showFitView showInteractive />
         
         <Background
           variant={BackgroundVariant.Dots}
